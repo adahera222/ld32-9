21ee6d00069ce61bb44e1d11c730bce073ed44bd
Jim MacArthur <jim@mode7.co.uk>
2013-12-14 11:53:26 +0000

    Added a wave of enemies

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 valuestorm: graphics
 
 graphics: FORCE
-	convert -background none -resize 50% graphics_in/ship.svg graphics/ship.png
-
+	convert -background none -resize 25% graphics_in/ship.svg graphics/ship.png
+	cp graphics_in/spr*.png graphics/
 FORCE:
--- valuestorm.js
+++ valuestorm.js
@@ -5,6 +5,11 @@ body = document.getElementsByTagName('body')[0];
 var keysDown = new Array();
 var x;
 var y;
+var frame = 0;
+
+function Enemy(type) {
+    this.type = type;
+}
 
 function getImage(name)
 {
@@ -12,31 +17,60 @@ function getImage(name)
     image.src = 'graphics/'+name+'.png';
     return image;
 }
+
+function makeWave() {
+    for(var i=0;i<5;i++) {
+	enemy = new Enemy(1);
+	enemy.x = 400+32*i;
+	enemy.y = 240;
+	enemies[enemies.length] = enemy;
+    }
+}
+
 function init()
 {
     shipImage = getImage("ship");
+    enemy1 = getImage("spr385283-29-22.600");
     x = 128;
     y = 128;
+    enemies = new Array();
+    makeWave();
     return true;
 }
 
 function draw() {
-  ctx.fillStyle = "#FF0000";
-  ctx.fillRect(0,0,640,480);
+  ctx.fillStyle = "#7F7fff";
+  ctx.fillRect(0,0,800,480);
   ctx.drawImage(shipImage, x, y);
+
+  for(var e=0;e<enemies.length;e++) {
+      en = enemies[e];
+      ctx.drawImage(enemy1, 24*Math.floor((frame/10)%5),0, 24,24, en.x-12, en.y-12, 24,24);
+  }
 }
 
 function processKeys() {
     if(keysDown[40]) {
-	y += 2;
+	y += 4;
     }
     if(keysDown[38]) {
-	y -= 2;
+	y -= 4;
+    }
+    if(keysDown[37]) {
+	x -= 4;
+    }
+    if(keysDown[39]) {
+	x += 4;
     }
 }
 
+function collisionDetector() {
+}
+
 function drawRepeat() {
+    frame += 1;
   processKeys();
+    collisionDetector();
   draw();
   setTimeout('drawRepeat()',20);
 }
